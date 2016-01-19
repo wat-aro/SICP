dc4d89834f3e1f1d0a11d4295d531a5014a4eac9
wat-aro <kazutas1008@gmail.com>
2016-01-19 12:22:25 +0900

    add gauche-compat-sicp & make stream-module

---
+++ sicp.scm
@@ -0,0 +1,62 @@
+;;;
+;;;  compat.sicp - some compatibility routines for those learning SICP
+;;;
+
+(define-module compat.sicp
+  (use srfi-27)
+  (export nil runtime true random false get put get-coercion put-coercion
+          cons-stream user-initial-environment extend))
+(select-module compat.sicp)
+
+;; This doesn't make nil as a boolean false, but in SICP nil is exclusively
+;; used to denote an empty list, so it's ok.
+(define nil '())
+
+;; Exercise 1.22
+(define (runtime)
+  (round->exact (* (expt 10 6)
+		   (time->seconds (current-time)))))
+
+;; Section 1.2
+(define true #t)
+(define (random n)
+  (random-integer n))
+
+;; Section 2.3
+;; Boolean false
+(define false #f)
+
+;; Section 2.4
+;; Symbol properties - a naive implementation
+(define *properties* (make-hash-table 'eq?))
+
+(define (put op type item)
+  (hash-table-update! *properties* op (^p (assoc-set! p type item)) '()))
+(define (get op type)
+  (assoc-ref (hash-table-get *properties* op '()) type #f))
+
+;; Section 2.5
+
+(define *coercions* (make-hash-table 'equal?))
+
+(define (put-coercion from to proc)
+  (hash-table-put! *coercions* (cons from to) proc))
+(define (get-coercion from to)
+  (hash-table-get *coercions* (cons from to) #f))
+
+;; Section 3.5.1
+
+(define-syntax cons-stream
+  (syntax-rules ()
+    [(_ a d) (cons a (delay d))]))
+
+;; Section 4.1.5
+(define user-initial-environment (interaction-environment))
+
+;; Section 4.4.4
+;; 'Extend' is bound to a syntax in Gauche's default environment, which
+;; may interfere with extend-if-consistent code; unless the user defines
+;; her own extend first, the compiler refers to the Gauche's extend and
+;; raises an error.   We export a dummy 'extend' binding so that
+;; the user won't be confused.
+(define extend #f)
---
+++ stream.scm
@@ -0,0 +1,64 @@
+(define-module stream.sicp
+  (export stream-null? stream-car stream-cdr delay cons-stream force memo-proc stream-map
+          stream-ref stream-for-each display-stream the-empty-stream stream-filter))
+
+(define (stream-null? stream)
+  (null? stream))
+
+(define (stream-car stream) (car stream))
+(define (stream-cdr stream) (force (cdr stream)))
+
+(define-syntax delay
+  (syntax-rules ()
+    ((_ exp) (memo-proc (lambda () exp)))))
+
+(define-syntax cons-stream
+  (syntax-rules ()
+    ((_ a b) (cons a (delay b)))))
+
+(define (force delayed-object)
+  (delayed-object))
+
+(define (memo-proc proc)
+  (let ((already-run? false) (result false))
+    (lambda ()
+      (if (not already-run?)
+          (begin (set! result (proc))
+                 (set! already-run? true)
+                 result)
+          result))))
+
+(define (stream-map proc . argstreams)
+  (if (stream-null? (car argstreams))
+      the-empty-stream
+      (cons-stream (apply proc (map stream-car argstreams))
+                   (apply stream-map
+                          (cons proc (map stream-cdr argstreams))))))
+
+(define (stream-ref s n)
+  (if (= n 0)
+      (stream-car s)
+      (stream-ref (stream-cdr s) (- n 1))))
+
+(define (stream-for-each proc s)
+  (if (stream-null? s)
+      'done
+      (begin (proc (stream-car s))
+             (stream-for-each proc (stream-cdr s)))))
+
+(define (display-stream s)
+  (stream-for-each display-line s))
+
+(define (display-line x)
+  (newline)
+  (display x))
+
+(define the-empty-stream '())
+
+(define (stream-filter pred stream)
+  (cond ((stream-null? stream) the-empty-stream)
+        ((pred (stream-car stream))
+         (cons-stream (stream-car stream)
+                      (stream-filter pred
+                                     (stream-cdr stream))))
+        (else (stream-filter pred (stream-cdr stream)))))
